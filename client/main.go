13a4fa7c75a6e4fadafb8eb2efa8a870088a71bb
xujiaxin <xujiaxin@beatosion.com>
2022-06-17 17:11:09 +0800

    handle EOF mistakes

--- client/main.go
+++ client/main.go
@@ -10,26 +10,27 @@ import (
 )
 
 func main() {
-	dl := websocket.Dialer{}
-	conn, _, err := dl.Dial("ws://127.0.0.1:8888", nil)
+	conn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8888", nil)
 	if err != nil {
-		log.Println(err)
+		log.Println("dial error:", err)
 		return
 	}
 	go send(conn)
 	for {
-		m, p, err := conn.ReadMessage()
+		_, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
 			break
 		}
-		fmt.Println(m, string(p))
+		fmt.Println(string(p))
 	}
 }
 func send(conn *websocket.Conn) {
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		l, _, _ := reader.ReadLine()
-		conn.WriteMessage(websocket.TextMessage, []byte(l))
+		conn.WriteMessage(websocket.TextMessage, l)
 	}
 }
